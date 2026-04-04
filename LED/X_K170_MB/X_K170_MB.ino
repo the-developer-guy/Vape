78828bbdf3dcdc2a7715a7ba579d909b17f6f893
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2026-04-04 15:20:24 +0200

    Added 4-flavor display pinout
    NOT WORKING, just for documentation

---
+++ LED/X_K170_MB/X_K170_MB.ino
@@ -0,0 +1,38 @@
+
+#define DISPLAY_BATT      (HLZZL) // HZZZL
+#define DISPLAY_LIQUID    (HLZLZ) // HZZLZ
+#define DISPLAY_PERCENT   (HLLZZ) // HZLZZ
+#define DISPLAY_1_A       (ZHLZZ)
+#define DISPLAY_1_B       (ZHZLZ)
+#define DISPLAY_2_A       (ZZHLZ)
+#define DISPLAY_2_B       (ZZLHZ)
+#define DISPLAY_2_C       (HLHZZ) // ZLHZZ
+#define DISPLAY_2_D       (HLZHZ) // ZLZHZ
+#define DISPLAY_2_E       (LZZHZ)
+#define DISPLAY_2_F       (lzhzz)
+#define DISPLAY_2_G       (LHZZZ)
+#define DISPLAY_3_A       (ZZZHL)
+#define DISPLAY_3_B       (ZZZLH)
+#define DISPLAY_3_C       (ZZHZL)
+#define DISPLAY_3_D       (ZZLZH)
+#define DISPLAY_3_E       (ZHZZL)
+#define DISPLAY_3_F       (HLZZH) // ZLZZH
+#define DISPLAY_3_G       (LZZZH)
+
+/**
+ A
+F B
+ G
+E C
+ D
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
