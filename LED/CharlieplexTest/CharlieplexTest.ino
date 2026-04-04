7451abbbcb3c24d827267b6334d5910c85e4bc15
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2026-04-04 14:23:44 +0200

    Added charlieplexing logic

--- LED/CharlieplexTest/CharlieplexTest.ino
+++ LED/CharlieplexTest/CharlieplexTest.ino
@@ -1,29 +1,62 @@
 
-uint8_t display = 0;
+#define HIZ_PIN (2)
+uint8_t display[5] = {0, 0, 0, 0, 0};
+char buff[10];
 
 void setup() {
   Serial.begin(115200);
-  for (int i = 2; i <= 8; i++) {
-    pinMode(i, OUTPUT);
-    digitalWrite(i, LOW);
-  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // manual stepping
   if (Serial.available()) {
     Serial.read();
-    display++;
-    Serial.println(display);
-    uint8_t currentDisplay = display;
-    for (uint8_t pin = 4; pin <= 8; pin++) {
-      if (currentDisplay & 0x01) {
-        digitalWrite(pin, HIGH);
-      } else {
-        digitalWrite(pin, LOW);
+
+
+    display[0]++;
+    for(int i = 0; i < 4; i++){
+      if(display[i] > HIZ_PIN){
+        display[i+1]++;
+        display[i] = 0;
       }
-      currentDisplay >>= 1;
     }
-    delay(1000);
+    if(display[4] > HIZ_PIN){
+      display[4] = 0;
+    }
+    sprintf(buff, "%c%c%c%c%c\n", translateState(display[4]),
+                                  translateState(display[3]),
+                                  translateState(display[2]),
+                                  translateState(display[1]),
+                                  translateState(display[0]));
+    Serial.print(buff);
+
+    for (int i = 4; i <= 8; i++) {
+      pinMode(i, INPUT);
+      digitalWrite(i, LOW);
+    }
+
+    uint8_t pin = 4;
+    for (int i = 0; i < 6; i++, pin++) {
+      if(display[i] == HIZ_PIN){
+        continue;
+      }
+      pinMode(pin, OUTPUT);
+      digitalWrite(pin, display[i]);
+    }
+  }
+}
+
+char translateState(int state){
+  if(state == LOW){
+    return 'L';
+  }
+  else if(state == HIGH){
+    return 'H';
+  }
+  else if(state == HIZ_PIN){
+    return 'Z';
+  }
+  else{
+    return 'E';
   }
 }
